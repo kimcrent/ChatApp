196854e36126908e8fe5d6a5e3852b1fdcc971e4
Roman Kim <kimcrent@gmail.com>
2024-04-12 22:02:59 +0600

    new class ChatServer

---
+++ src/com.kimcrent.javachatapp/ChatServer.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.net.*;
+
+public class ChatServer{
+    public static void main(String[] args) throws IOException{
+        ServerSocket serverSocket = new ServerSocket(2000);
+        System.out.println("Server started. Waiting for clients ...");
+        
+    }
+}
