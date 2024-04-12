75a0b0977b4a002beec9a6c2aed816f52934a75c
Roman Kim <kimcrent@gmail.com>
2024-04-12 22:21:37 +0600

    create class chat server

--- src/com.kimcrent.javachatapp/ChatServer.java
+++ src/com.kimcrent.javachatapp/ChatServer.java
@@ -1,10 +1,27 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class ChatServer{
-    public static void main(String[] args) throws IOException{
-        ServerSocket serverSocket = new ServerSocket(2000);
-        System.out.println("Server started. Waiting for clients ...");
-        
+    // List to keep track of all connected clients
+    private static List<ClientHandler> clients = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(5000);
+        System.out.println("Server started. Waiting for clients...");
+
+        while(true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected: " + clientSocket);
+
+            // Spawn a new thread for each client
+            ClientHandler clientThread = new ClientHandler(clientSocket, clients);
+            clients.add(clientThread);
+            new Thread(ckientTread).start();
+        }
     }
+}
+
+class clientHandler implements Runnable{
+    
 }
